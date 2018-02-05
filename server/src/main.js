3adf8c379a55cce9fcd05e4a6bdcaf455156bb3e
Manoel Jr <manoeljnr@gmail.com>
2018-02-05 00:25:23 -0300

    Fix main.js to search issues in database first

--- server/src/main.js
+++ server/src/main.js
@@ -6,6 +6,7 @@ const cors = require("cors")
 const app = express()
 const axios = require('axios')
 const _ = require('lodash');
+let issues = []
 
 app.use(cors())
 app.use(morgan('dev'))
@@ -15,23 +16,30 @@ const api = axios.create({
   baseURL: 'https://api.github.com'
 })
 
-// Search for issues and insert into database
-api.get('/repos/sombriks/hello-js-v5/issues').then(result => {
-  let issues = result.data.map(item => {
-    return _.pick(item, ['id', 'title', 'number'])
-  })
 
-  knex('issues').count('id as id').then(res => {
-    //console.log(res)
-    if (res[0].id === 0) {
-      return knex('issues').insert(issues)
+// Search user's comments for each issue and save into database
+app.post('/check/:user', (req, res) => {
+
+})
+
+knex.migrate.latest().then( () => {
+  // Search for issues and insert into database
+  knex('issues').select().then(res => {
+    if (res.length == 0) {
+      api.get('/repos/sombriks/hello-js-v5/issues').then(result => {
+        issues = result.data.map(item => {
+          return _.pick(item, ['id', 'title', 'number'])
+        })
+        return knex('issues').insert(issues)
+      }).then(res => {
+        if(res) console.log('Issues inserted successfully')
+      })
+    } else {
+      console.log(res)
     }
-  }).then(res => {
-    if(res) console.log('Issues inserted successfully')
   })
-});
 
-knex.migrate.latest().then( _ => {
+  // Start express server
   app.listen(3000, _ => {
     console.log('Server is running...')
   });
