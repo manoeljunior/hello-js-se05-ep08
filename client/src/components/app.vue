7ec5bd056488bd63d86fdd8bcc59c946bc663ed7
Manoel Francisco Freitas Junior - DATAPREVCE <manoel.freitasjunior@dataprev.gov.br>
2018-02-05 16:25:58 -0300

    Creating app.vue

---
+++ client/src/components/app.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <md-layout md-align="center" md-gutter="20">
+      <md-layout md-align="center" md-flex="100">
+        <h1>Final project - Hello.js</h1>
+      </md-layout>
+  
+      <md-layout md-flex-small="100" md-flex="50">
+        <form @submit.prevent="check">
+          <md-input-container>
+            <label>Usuário</label>
+            <md-input v-model="user" required></md-input>
+          </md-input-container>
+        </form>
+      </md-layout>
+      <router-view></router-view>
+    </md-layout>
+    
+  </div>
+</template>
+
+<script>
+const VueRouter = require("vue-router");
+
+module.exports = {
+  name: "App",
+  data () {
+    return {
+      user: 'tesst'
+    }
+  },
+  methods: {
+    check() {
+
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
