a5a646353f24ac8f3b692f5920f6b66aae33a7f8
Manoel Jr <manoeljnr@gmail.com>
2018-02-08 23:43:39 -0300

    refactoring of server project

--- server/src/main.js
+++ server/src/main.js
@@ -7,7 +7,6 @@ const app = express()
 const axios = require('axios')
 const _ = require('lodash');
 const {extractComments} = require('./util')
-const {extractAllComments} = require('./util')
 let issues = []
 
 app.use(cors())
@@ -18,42 +17,7 @@ const api = axios.create({
   baseURL: 'https://api.github.com'
 })
 
-app.get('/list', (req, res) => {
-  let obj = {
-    user: '',
-    comments: []
-  }
-  let comments = []
-  let newResult = []
-  let userGroup = ''
-  knex('comments')
-    .join('users', 'comments.id_user', '=', 'users.id_user')
-    .join('issues', 'comments.id_issue', '=', 'issues.id')
-    .where('comments.body', 'like', '%present%')
-    .select('comments.body', 'users.name', 'issues.title', 'comments.id_issue')
-
-    .orderBy('users.name').then(result => {
-      result.forEach(item => {
-        if(userGroup != item.name) {
-          obj.comments = extractComments(result, item.name)
-          obj.user = item.name
-          newResult.push(obj)
-          userGroup = item.name
-        } 
-        //obj.comments = comments
-        console.log('object ***')
-        console.log(obj)
-
-      })
-      console.log('newresult')
-      console.log(newResult)
-      console.log(newResult[0].comments)
-      res.send(result)
-    })
-})
-
 app.get('/listall', (req, res) => {
-  let obj = { }
   let comments = []
   let newResult = []
   let userGroup = ''
@@ -65,170 +29,30 @@ app.get('/listall', (req, res) => {
     .orderBy('users.name').then(result => {
       result.forEach(item => {
         if(userGroup != item.name) {
-          //obj.comments = extractAllComments(result, item.name)
-          //obj.user = item.name
           newResult.push({
             user: item.name,
-            comments: extractAllComments(result, item.name)
+            comments: extractComments(result, item.name)
           })
           userGroup = item.name
-        } 
-        //obj.comments = comments
-        console.log('object ***')
-        console.log(obj)
-
+        }
       })
-      console.log('newresult')
-      console.log(newResult)
-      console.log(newResult[0].comments)
       res.send(newResult)
     })
 })
 
 // Search user's comments for each issue and save into database
-app.post('/check/:user', (req, res) => {
-
-  // issues = [
-  //   { id: 291579075, number: 3, title: 'SE05EP01' },
-  //   { id: 291579364, number: 4, title: 'SE05EP02' },
-  //   { id: 291579687, number: 5, title: 'SE05EP02' }
-  // ]
-
-  let f = function(issue) {
-    //console.log(`/repos/sombriks/hello-js-v5/issues/${issue.number}/comments`)
-    return api.get(`/repos/sombriks/hello-js-v5/issues/${issue.number}/comments`)
-  }
-
-  let urlCall = []
-  
-
-  let userComments = { 
-     user: req.params.user,
-     comments: [] 
-  }
-  //let issueUserComments = []
-  let object = []
-  let msg = 'user not found'
-  
-  console.log(issues)
-  // issues.forEach(issue => {
-  //   urlCall.push(() => {
-  //     return api.get(`/repos/sombriks/hello-js-v5/issues/${issue.number}/comments`)
-  //   })()
-  // })
- 
-  Promise.all([f(issues[0]), f(issues[1]), f(issues[2]), f(issues[3]), f(issues[4]), f(issues[5]), f(issues[6]), f(issues[7])]).then(results => {
-    console.log('Promise all')
-    console.log(results[0])
-    results.forEach((result, idx) => {
-      let issueUserComments = result.data.filter(item => {
-        return item.user.login === req.params.user
-      })
-
-      if (issueUserComments.length > 0) {
-        issueUserComments.forEach(item => {
-          item.id_issue = issues[idx].id
-          item.id_user = item.user.id
-          item.body = item.body
-          userComments.comments.push(_.pick(item, ['id_issue', 'id_user', 'body', 'user' ]))
-        })
-      }
-
-      // console.log('issueUserComments ****')
-      // console.log(issueUserComments)
-
-      // console.log('userComments ***')
-      // console.log(userComments)
-
-    })
-
-    // persist the user and comments
-    if (userComments.comments.length > 0) {
-      knex('users').select().where('name', userComments.user).then(user => {
-        console.log('consulta')
-        console.log(user)
-        // Dont' save the user if exists
-        if (user.length == 0) {
-          knex('users').insert({
-            id_user: userComments.comments[0].user.id,
-            name: userComments.comments[0].user.login,
-            avatar: userComments.comments[0].user.avatar_url,
-          }).then(userInserted => {
-            console.log('userInserted')
-            console.log(userInserted)
-            let comments = []
-            userComments.comments.forEach(comment => {
-              let obj = _.pick(comment, ['id_issue', 'id_user', 'body'])
-              comments.push(obj)
-            })
-            knex('comments').insert(comments).then(commentsInserted => {
-              console.log('commentsInserted')
-              console.log(commentsInserted)
-              if(commentsInserted.length > 0) {
-                console.log(commentsInserted)
-                res.send({
-                  msg: 'Comments inserted succesfully'
-                })
-              }
-            })
-          })
-        } else {
-          res.send({
-            msg: 'User already saved'
-          })
-        }
-      })  
-    } else {
-      res.send({
-        msg: 'There is no comments'
-      })
-    }
-    // console.log(result[0].data)
-    // console.log(result[1].data)
-  }).catch(err => {
-    console.log(err)
-  })
-  //res.send({msg})
-})
-
-
 app.post('/check', (req, res) => {
-  console.log('issues')
-  console.log(issues)
   let user = req.body.user
-  console.log(req.body)
-  // issues = [
-  //   { id: 291579075, number: 3, title: 'SE05EP01' },
-  //   { id: 291579364, number: 4, title: 'SE05EP02' },
-  //   { id: 291579687, number: 5, title: 'SE05EP02' }
-  // ]
-
   let f = function(issue) {
-    console.log(`/repos/sombriks/hello-js-v5/issues/${issue.number}/comments`)
     return api.get(`/repos/sombriks/hello-js-v5/issues/${issue.number}/comments`)
   }
-
-  let urlCall = []
-  
 
   let userComments = { 
      user: user,
      comments: [] 
   }
-  //let issueUserComments = []
-  let object = []
-  let msg = 'user not found'
   
-  console.log(issues)
-  // issues.forEach(issue => {
-  //   urlCall.push(() => {
-  //     return api.get(`/repos/sombriks/hello-js-v5/issues/${issue.number}/comments`)
-  //   })()
-  // })
- 
   Promise.all([f(issues[0]), f(issues[1]), f(issues[2]), f(issues[3]), f(issues[4]), f(issues[5]), f(issues[6]), f(issues[7])]).then(results => {
-    console.log('Promise all')
-    console.log(results[0])
     results.forEach((result, idx) => {
       let issueUserComments = result.data.filter(item => {
         return item.user.login === user
@@ -242,20 +66,11 @@ app.post('/check', (req, res) => {
           userComments.comments.push(_.pick(item, ['id_issue', 'id_user', 'body', 'user' ]))
         })
       }
-
-      // console.log('issueUserComments ****')
-      // console.log(issueUserComments)
-
-      // console.log('userComments ***')
-      // console.log(userComments)
-
     })
 
     // persist the user and comments
     if (userComments.comments.length > 0) {
       knex('users').select().where('name', userComments.user).then(user => {
-        console.log('consulta')
-        console.log(user)
         // Dont' save the user if exists
         if (user.length == 0) {
           knex('users').insert({
@@ -263,18 +78,13 @@ app.post('/check', (req, res) => {
             name: userComments.comments[0].user.login,
             avatar: userComments.comments[0].user.avatar_url,
           }).then(userInserted => {
-            console.log('userInserted')
-            console.log(userInserted)
             let comments = []
             userComments.comments.forEach(comment => {
               let obj = _.pick(comment, ['id_issue', 'id_user', 'body'])
               comments.push(obj)
             })
             knex('comments').insert(comments).then(commentsInserted => {
-              console.log('commentsInserted')
-              console.log(commentsInserted)
               if(commentsInserted.length > 0) {
-                console.log(commentsInserted)
                 res.send({
                   msg: 'Comments inserted succesfully'
                 })
@@ -292,12 +102,12 @@ app.post('/check', (req, res) => {
         msg: 'There is no comments'
       })
     }
-    // console.log(result[0].data)
-    // console.log(result[1].data)
   }).catch(err => {
-    console.log(err)
+    console.log(err);
+    res.status(err.response.status).send({
+      msg: err.response.data.message
+    })
   })
-  //res.send({msg})
 })
 
 knex.migrate.latest().then( () => {
@@ -310,7 +120,6 @@ knex.migrate.latest().then( () => {
         }).filter(item => {
           return (/SE05EP0([1-8])/).test(item.title)
         })
-        console.log(`ISSUES -> ${issues}`)
         return knex('issues').insert(issues)
       }).then(res => {
         if(res) console.log('Issues inserted successfully')
--- server/src/util.js
+++ server/src/util.js
@@ -1,17 +1,4 @@
 exports.extractComments = function(result, user) {
-  let comments = []
-  result.forEach(item => {
-    if(user == item.name) {
-      comments.push({
-        title: item.title, 
-        body: item.body
-      })
-    } 
-  })
-  return comments
-}
-
-exports.extractAllComments = function(result, user) {
   let comments = {}
   result.forEach(item => {
     if(user == item.name) {
