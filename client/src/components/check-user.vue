e1aca5ec91d7750235cc0dda466e99e021e79718
Manoel Jr <manoeljnr@gmail.com>
2018-02-09 09:42:15 -0300

    change buttons

--- client/src/components/check-user.vue
+++ client/src/components/check-user.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="check-user">
-      <form>
-        <md-layout md-gutter>
-          <md-layout md-flex="50">
-            <md-input-container>
-              <label>Usuário</label>
-              <md-input v-model="user" class="md-inline"></md-input>
-              <br>
-            </md-input-container>
-            <md-progress v-if="searching" md-indeterminate></md-progress>
-            <label id="message">{{msg}}</label>
-          </md-layout>
-          <md-layout>
-            <md-button @click="check" class="md-primary">
-              <md-icon>find_in_page</md-icon>
-              <md-tooltip md-direction="top">Search for a user</md-tooltip>
-            </md-button>
-          </md-layout>      
+    <form>
+      <md-layout md-gutter>
+        <md-layout md-flex="100">
+          <md-input-container>
+            <label>Aluno</label>
+            <md-input v-model="user" class="md-inline"></md-input>
+            <br>
+          </md-input-container>
+          <md-progress v-if="searching" md-indeterminate></md-progress>
+          <label id="message">{{msg}}</label>
         </md-layout>
-      </form>
+      </md-layout>
+    </form>
     <md-layout>
-      <md-button class="md-primary" @click="listUsers">
+      <md-button @click="check" class="md-primary md-raised">
+        <md-icon>find_in_page</md-icon>
+        <md-tooltip md-direction="right">Search for a user</md-tooltip>
+      </md-button>
+      <md-button @click="listUsers" class="md-primary md-raised">
         <md-icon>playlist_add_check</md-icon>
-        <md-tooltip md-direction="top">List all users inserted</md-tooltip>
+        <md-tooltip md-direction="right">List all users inserted</md-tooltip>
       </md-button>
     </md-layout>
   </div>  
@@ -47,6 +45,7 @@ module.exports = {
   methods: {
     check() {
       this.searching = true;
+      this.msg = ''
       api.post('/check', {user: this.user}).then(ret => {
         this.msg = ret.data.msg;
         this.searching = false;
@@ -54,7 +53,6 @@ module.exports = {
         this.msg = err.response.data.msg;
         this.searching = false;
       })
-
     },
     listUsers() {
       this.$router.push('/list-users')
@@ -64,12 +62,6 @@ module.exports = {
 </script>
 
 <style scoped>
-  #app {
-    margin-top: 30px;
-    margin-left: 100px;
-    margin-right: 100px;
-  }
-
   #message {
     color: #aaa;
     font-size: 14px;
